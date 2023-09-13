121dbf7b5c9ea0c44a9efe23ce783df0032bb5e5
Griffin Fore <griffinfore@gmail.com>
2023-09-13 12:24:04 -0700

    It is what it is

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,45 +1,34 @@
 from flask_app import app
 from flask import Flask, render_template, redirect, request, session
 from flask_app.models import user
+from flask_bcrypt import Bcrypt
 
 # READ
 @app.route('/')
 def index():
-    users = user.User.get_all()
-    print(users)
-    return render_template("index.html", all_users = users)
+    return render_template("index.html")
 
-@app.route('/users/new')
-def new_user():
-    return render_template('create_user_form.html')
 
 # CREATE
 @app.route('/create_user', methods=["POST"])
 def create_user():
-    user.User.save(request.form)
-    return redirect('/')
+    user.User.create_user(request.form)
+    return redirect('/user')
 
 #show one user route page
-@app.route('/users/<int:user_id>')
-def show_one_user(user_id):
-    user1 = user.User.show_one_user(user_id)
-    return render_template('one_user.html', user=user1)
+@app.route('/user')
+def show_one_user():
+    if 'user_id' not in session:
+        return redirect('/')
+    return render_template('one_user.html')
 
-
-#edit user route page
-@app.route('/users/<int:user_id>/edit')
-def edit_one_user(user_id):
-    user1 = user.User.show_one_user(user_id)
-    return render_template('edit_user.html', user=user1)
-
-#edit user database route
-@app.route('/users/edit', methods=["POST"])
-def update_user():
-    user.User.update(request.form)
+@app.route('/users/login', methods=["POST"])
+def login():
+    if user.User.login(request.form):
+        return redirect('/user')
     return redirect('/')
 
-#delete user database route
-@app.route('/delete/<int:user_id>')
-def delete_user(user_id):
-    user.User.delete(user_id)
+@app.route('/logout')
+def logout():
+    session.clear()
     return redirect('/')
--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -2,58 +2,108 @@
 from flask_app import app
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash, session
+import re
+from flask_bcrypt import Bcrypt
 
+bcrypt = Bcrypt(app)
 class User:
-    DB = 'users_cr'
+    DB = 'login_and_registration'
+
     def __init__( self , data ):
         self.id = data['id']
         self.first_name = data['first_name']
         self.last_name = data['last_name']
         self.email = data['email']
+        self.password = data['password']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
-    # Now we use class methods to query our database
-
-    #Read
-    @classmethod
-    def get_all(cls):
-        query = "SELECT * FROM users;"
-        # make sure to call the connectToMySQL function with the schema you are targeting.
-        results = connectToMySQL(cls.DB).query_db(query)
-        # Create an empty list to append our instances of friends
-        users = []
-        # Iterate over the db results and create instances of friends with cls.
-        for user in results:
-            users.append(cls(user))
-        return users
-    
-    # get one user
-    @classmethod
-    def show_one_user(cls, user_id):
-        query = "SELECT * FROM users WHERE id = %(user_id)s;"
-        data = {"user_id": user_id }
-        results = connectToMySQL(cls.DB).query_db(query, data)
-        return cls(results[0])
 
     # Create/Insert
     @classmethod
-    def save(cls, data):
-        query = """INSERT INTO users (first_name, last_name, email, created_at, updated_at) 
-        VALUES (%(first_name)s, %(last_name)s, %(email)s, NOW(), NOW());"""
-        return connectToMySQL(cls.DB).query_db(query, data)
-    
-    # update one user
+    def create_user(cls, user_data):
+        if not cls.validate_user_data(user_data):
+            return False
+        # cls.parse_user_data(user_data)
+        user_data = user_data.copy()
+        user_data['password'] =  bcrypt.generate_password_hash(user_data['password'])
+        query = """
+        INSERT INTO users (
+            first_name, 
+            last_name, 
+            email, 
+            password) 
+        VALUES (
+            %(first_name)s, 
+            %(last_name)s, 
+            %(email)s,
+            %(password)s)
+        ;"""
+        user_id = connectToMySQL(cls.DB).query_db(query, user_data)
+        session['user_id'] = user_id
+        session['user_name'] = f'{user_data["first_name"]} {user_data["last_name"]}'
+        return user_id
+        
     @classmethod
-    def update(cls, data):
-        query = """UPDATE users
-            SET first_name= %(first_name)s, last_name= %(last_name)s, email= %(email)s, updated_at= NOW()
-            WHERE id= %(id)s;"""
-        return connectToMySQL(cls.DB).query_db(query, data)
+    def validate_user_data(cls, data):
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        is_valid = True
+        if len(data['first_name']) < 2:
+            flash('First name must contain at least two characters!')
+            is_valid = False
+        if len(data['last_name']) < 2:
+            flash('Last name must contain at least 2 characters!')
+            is_valid = False
+        # if 'id' not in data:
+        if len(data['password']) < 8:
+            flash('Password must be at least eight characters')
+            is_valid = False
+        if data['password'] != data['confirm_password']:
+            flash('Passwords do not match')
+            is_valid = False
+        # if 'id' not in data or data['email'] != User.get_user_by_id(data[id]).email : #if new user or if user wants to change email
+        if not EMAIL_REGEX.match(data['email']):
+            flash('invalid email address')
+            is_valid = False
+        if User.get_user_by_email(data['email']):
+            flash('Email already taken')
+            is_valid = False
+        return is_valid
 
-    # delete one user
+    # @staticmethod
+    # def parse_user_data(data):
+    #     parsed_data = {
+    #         'email' : data['email'],
+    #         'first_name' : data['first_name'],
+    #         'last_name' : data['last_name'],
+    #         'password' : bcrypt.generate_password_hash(data['password'])
+    #     }
+    #     print('88888888888888888 Parsed_Data', parsed_data)
+    #     return parsed_data
+
     @classmethod
-    def delete(cls, user_id):
-        query = """ DELETE FROM users WHERE id = %(id)s;"""
-        data = { "id" : user_id }
-        return connectToMySQL(cls.DB).query_db(query, data)
+    def get_user_by_email(cls, email):
+        query = "SELECT * FROM users WHERE email = %(email)s"
+        data = { 'email' : email }
+        user_data = connectToMySQL(cls.DB).query_db(query, data)
+        if not user_data:
+            return False
+        return cls(user_data[0])
     
+    @classmethod
+    def get_user_by_id(cls, id):
+        query = "SELECT * FROM users WHERE id = %(id)s"
+        data = { 'id' : id }
+        user_data = connectToMySQL(cls.DB).query_db(query, data)
+        return cls(user_data[0])
+    
+    @staticmethod
+    def login(data):
+        this_user = User.get_user_by_email(data['email'])
+        print('this user = ', this_user)
+        if this_user:
+            if bcrypt.check_password_hash(this_user.password, data['password']):
+                session['user_id'] = this_user.id
+                session['user_name'] = f'{this_user.first_name} {this_user.last_name}'
+                return True
+        flash('Email or Password incorrect')
+        return False
